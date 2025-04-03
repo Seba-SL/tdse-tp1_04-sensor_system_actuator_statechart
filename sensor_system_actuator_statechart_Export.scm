ba7d085345713f846427a70762a40ecf111f2296
Seba-SL <slazo@fi.uba.ar>
2025-04-03 07:50:53 -0300

    first commit

---
+++ sensor_system_actuator_statechart_Export.scm
@@ -0,0 +1,501 @@
+{
+  "graph": {
+    "cells": [
+      {
+        "position": {
+          "x": 0,
+          "y": 0
+        },
+        "size": {
+          "height": 10,
+          "width": 10
+        },
+        "angle": 0,
+        "type": "Statechart",
+        "id": "78749915-0da0-40a2-862f-9e8d94c7c68e",
+        "linkable": false,
+        "z": 1,
+        "attrs": {
+          "name": {
+            "text": "sensor_system_actuator_statechart Export"
+          },
+          "specification": {
+            "text": "@EventDriven\n@SuperSteps(no)\n\ninterface: \n    in event EV_LED_01_ON\n    in event EV_LED_01_OFF\n"
+          }
+        }
+      },
+      {
+        "type": "Entry",
+        "position": {
+          "x": 222.5,
+          "y": 130
+        },
+        "size": {
+          "height": 15,
+          "width": 15
+        },
+        "angle": 0,
+        "entryKind": "Initial",
+        "fixedRatio": true,
+        "embedable": false,
+        "linkable": true,
+        "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "z": 2,
+        "embeds": [
+          "f70f632c-e54d-4002-9ec5-7baec620de10"
+        ],
+        "attrs": {
+          "name": {
+            "fill": "#555555"
+          }
+        }
+      },
+      {
+        "type": "NodeLabel",
+        "label": true,
+        "size": {
+          "width": 15,
+          "height": 15
+        },
+        "position": {
+          "x": 222.5,
+          "y": 145
+        },
+        "id": "f70f632c-e54d-4002-9ec5-7baec620de10",
+        "z": 3,
+        "parent": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "attrs": {
+          "label": {
+            "refX": "50%",
+            "textAnchor": "middle",
+            "refY": "50%",
+            "textVerticalAnchor": "middle"
+          }
+        }
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 170,
+          "y": 210
+        },
+        "size": {
+          "width": 120,
+          "height": 60
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+        "z": 10,
+        "embeds": [
+          "f1854392-79fb-425c-a03e-fe4ce24207b9"
+        ],
+        "attrs": {
+          "name": {
+            "text": "ST_LED_01_OFF"
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {},
+            "position": {}
+          },
+          {
+            "attrs": {
+              "root": {
+                "opacity": 1
+              },
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "9b5ce41b-0116-46a8-a603-ea4119c6e5d7",
+        "z": 11,
+        "attrs": {}
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 180,
+          "y": 520
+        },
+        "size": {
+          "width": 120,
+          "height": 60
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "db372094-acc6-467b-b44d-495606208445",
+        "z": 15,
+        "embeds": [
+          "ac6609d6-ecb7-4450-b861-84db04e8130d"
+        ],
+        "attrs": {
+          "name": {
+            "text": "ST_LED_01_ON"
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 120,
+              "dy": 37,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_01_OFF\n"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "root": {
+                "opacity": 1
+              },
+              "label": {
+                "text": "2"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "77eb8e24-df92-49c9-8573-5d74a5842d04",
+        "z": 16,
+        "vertices": [
+          {
+            "x": 360,
+            "y": 230
+          },
+          {
+            "x": 440,
+            "y": 370
+          }
+        ],
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 10,
+              "dy": 37,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_01_ON"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "root": {
+                "opacity": 1
+              },
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "6ba688fb-d6e3-41fe-ad0d-510ce6d63764",
+        "z": 16,
+        "vertices": [
+          {
+            "x": 160,
+            "y": 230
+          },
+          {
+            "x": 0,
+            "y": 330
+          }
+        ],
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 30,
+              "dy": 60,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_01_OFF"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "root": {
+                "opacity": 1
+              },
+              "label": {
+                "text": "3"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "f1854392-79fb-425c-a03e-fe4ce24207b9",
+        "z": 17,
+        "parent": "36b8548a-74b1-4925-ae23-291c03231ee6",
+        "vertices": [
+          {
+            "x": 260,
+            "y": 320
+          }
+        ],
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 100,
+              "dy": 0,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 40,
+              "dy": 10,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_LED_01_ON"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "root": {
+                "opacity": 1
+              },
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "ac6609d6-ecb7-4450-b861-84db04e8130d",
+        "z": 18,
+        "parent": "db372094-acc6-467b-b44d-495606208445",
+        "vertices": [
+          {
+            "x": 280,
+            "y": 480
+          },
+          {
+            "x": 220,
+            "y": 480
+          }
+        ],
+        "attrs": {}
+      }
+    ]
+  },
+  "genModel": {
+    "generator": {
+      "type": "create::c",
+      "features": {
+        "Outlet": {
+          "targetProject": "",
+          "targetFolder": "",
+          "libraryTargetFolder": "",
+          "skipLibraryFiles": "",
+          "apiTargetFolder": ""
+        },
+        "LicenseHeader": {
+          "licenseText": ""
+        },
+        "FunctionInlining": {
+          "inlineReactions": false,
+          "inlineEntryActions": false,
+          "inlineExitActions": false,
+          "inlineEnterSequences": false,
+          "inlineExitSequences": false,
+          "inlineChoices": false,
+          "inlineEnterRegion": false,
+          "inlineExitRegion": false,
+          "inlineEntries": false
+        },
+        "OutEventAPI": {
+          "observables": false,
+          "getters": false
+        },
+        "IdentifierSettings": {
+          "moduleName": "MyFirstStatechart",
+          "statemachinePrefix": "myFirstStatechart",
+          "separator": "_",
+          "headerFilenameExtension": "h",
+          "sourceFilenameExtension": "c"
+        },
+        "Tracing": {
+          "enterState": "",
+          "exitState": "",
+          "generic": ""
+        },
+        "Includes": {
+          "useRelativePaths": false,
+          "generateAllSpecifiedIncludes": false
+        },
+        "GeneratorOptions": {
+          "userAllocatedQueue": false,
+          "metaSource": false
+        },
+        "GeneralFeatures": {
+          "timerService": false,
+          "timerServiceTimeType": ""
+        },
+        "Debug": {
+          "dumpSexec": false
+        }
+      }
+    }
+  }
+}
